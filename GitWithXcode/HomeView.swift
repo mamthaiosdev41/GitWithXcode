46d70473923d20d4847854b6f6ea92c1646142c6
Mamtha <mamtha.iosdev41@gmail.com>
2026-04-01 15:05:31 +0530

    Added HomeView File

---
+++ GitWithXcode/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GitWithXcode
+//
+//  Created by Apple on 01/04/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
