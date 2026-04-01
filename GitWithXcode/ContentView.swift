c94b327fb609a9217ec153dd3b1d51f60870ea31
Mamtha <mamtha.iosdev41@gmail.com>
2026-04-01 14:53:29 +0530

    My first Commit

--- GitWithXcode/ContentView.swift
+++ GitWithXcode/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
+                .font(Font.largeTitle)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("SwiftFul Thinking!")
         }
         .padding()
     }
