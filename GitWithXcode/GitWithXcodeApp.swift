5995836f4a04cbaaf4f39cbcc3e5df3bc6bdfd66
Mamtha <mamtha.iosdev41@gmail.com>
2026-04-01 14:27:15 +0530

    Initial Commit

---
+++ GitWithXcode/GitWithXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitWithXcodeApp.swift
+//  GitWithXcode
+//
+//  Created by Apple on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct GitWithXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
